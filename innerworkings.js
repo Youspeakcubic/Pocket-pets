910c38127b1f89752fba718f2f48d56276c738eb
Anthony <delantho327@gmail.com>
2021-07-27 03:59:10 -0500

    need to use waapi to add options for animations.

--- innerworkings.js
+++ innerworkings.js
@@ -1,3 +1,4 @@
+//counter for 1 by 1 addition of progress bar number.
 const numb = document.querySelector(".number");
 let counter = 0;
 setInterval(() => {
@@ -8,3 +9,6 @@ setInterval(() => {
     numb.textContent = counter + "%";
   }
 }, 80);
+// animation addition and removal for progress bars based on value.
+let progress = document.getElementById('progress')
+progress.classlist.add("progressA")
