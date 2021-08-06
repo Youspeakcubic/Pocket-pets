f3d4990367eeb86e021cdffab5d2d92d94db282c
Anthony <delantho327@gmail.com>
2021-08-06 04:49:45 -0500

    changed progress bars and added system to account for time passed for attributes to diminish, added alert for when traits were low. Will add shop and sprites for food water and toys.

--- innerworkings.js
+++ innerworkings.js
@@ -1,14 +1,23 @@
-//counter for 1 by 1 addition of progress bar number.
-const numb = document.querySelector(".number");
-let counter = 0;
-setInterval(() => {
-  if(counter == 100 ){
-    clearInterval();
-  }else{
-    counter+=1;
-    numb.textContent = counter + "%";
-  }
-}, 80);
-// animation addition and removal for progress bars based on value.
-let progress = document.getElementById('progress')
-progress.classlist.add("progressA")
+let count = 0;
+let hunger = 100;
+let thirst = 100;
+let happiness = 0;
+let coins = 100;
+let hungerProgress = document.getElementById('progressBarHung')
+let thirstProgress = document.getElementById('progressBarThir')
+let happinessProgress = document.getElementById('progressBarHapp')
+let time = () => {
+if(hunger <= 0 || thirst <= 0) {
+  alert("Your pet is dying, please take care of them ;(")
+}
+else{
+  console.log("gay");
+  thirst -= .5
+  hunger -= 1.5
+  happiness = (thirst + hunger)/2
+  console.log(hunger, happiness, thirst);
+  hungerProgress.style.width = hunger + "%";
+  thirstProgress.style.width = thirst + "%";
+  happinessProgress.style.width = happiness + "%";
+}};
+setInterval(time, 10000);
