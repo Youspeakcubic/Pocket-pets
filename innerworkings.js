6d36d8523d708f3e358f79ff7415213d3590495f
Anthony <delantho327@gmail.com>
2021-08-12 06:58:44 -0500

    added poop and pee generation that randomly spawns in play area, clicking on these will give you 20 coins, next i will add animations for food and water. adding more items will come after that.

--- innerworkings.js
+++ innerworkings.js
@@ -7,6 +7,11 @@ let hungerProgress = document.getElementById('progressBarHung');
 let thirstProgress = document.getElementById('progressBarThir');
 let happinessProgress = document.getElementById('progressBarHapp');
 let coinTab = document.getElementById('coinsTab');
+let poopSprite = document.getElementById('dogPoop');
+let peeSprite = document.getElementById('dogPee');
+let randomNumber = (min, max) =>{
+  return Math.random() * (max-min) + min;
+}
 
 let time = () => {
   if(hunger <= 0 || thirst <= 0) {
@@ -22,7 +27,32 @@ let time = () => {
     happinessProgress.style.width = happiness + "%";
   }};
 
-setInterval(time, 10000);
+let poopGen = () => {
+  let fecesType = Math.round(randomNumber(0,2));
+  if (fecesType == 1) {
+    console.log("poop1");
+    poopSprite.style.left = randomNumber(50, 850) + "px";
+    poopSprite.style.top = randomNumber(20,250) + "px";
+    poopSprite.style.display = "block";
+  }
+  else if (fecesType == 2){
+    console.log("poop2");
+    peeSprite.style.left = randomNumber(50, 850) + "px";
+    peeSprite.style.top = randomNumber(20,250) + "px";
+    peeSprite.style.display = "block";
+  }
+}
+
+let poopPickup = (type) => {
+  coins += 20;
+  coinTab.textContent = coins
+  if (type == 1){
+    poopSprite.style.display = "none";
+  }
+  else if (type == 2) {
+    peeSprite.style.display = "none;"
+  }
+}
 
 let replenish = (type) => {
   if(type == 1 ) {
@@ -50,3 +80,5 @@ let replenish = (type) => {
     coinTab.textContent = coins
   }
 };
+setInterval(time, 10000);
+setInterval(poopGen,40000);
