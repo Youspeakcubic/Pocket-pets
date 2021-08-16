5d0a7cdf5a0e58b89c7c521d74ce831033e0f6a8
Anthony <delantho327@gmail.com>
2021-08-16 07:27:05 -0500

    Added animation for dog to go to bowls when food or water is bought, also fixed issue with poops showing up all over the place. Next will be adding more items and maybe a walk feature.

--- innerworkings.js
+++ innerworkings.js
@@ -3,12 +3,15 @@ let hunger = 100;
 let thirst = 100;
 let happiness = 0;
 let coins = 100;
+let dog = document.getElementById('dog');
 let hungerProgress = document.getElementById('progressBarHung');
 let thirstProgress = document.getElementById('progressBarThir');
 let happinessProgress = document.getElementById('progressBarHapp');
 let coinTab = document.getElementById('coinsTab');
 let poopSprite = document.getElementById('dogPoop');
 let peeSprite = document.getElementById('dogPee');
+let foodBowl = document.getElementById('dogFood');
+let waterBowl = document.getElementById('dogWater');
 let randomNumber = (min, max) =>{
   return Math.random() * (max-min) + min;
 }
@@ -31,14 +34,14 @@ let poopGen = () => {
   let fecesType = Math.round(randomNumber(0,2));
   if (fecesType == 1) {
     console.log("poop1");
-    poopSprite.style.left = randomNumber(50, 850) + "px";
-    poopSprite.style.top = randomNumber(20,250) + "px";
+    poopSprite.style.left = randomNumber(22, 50) + "vw";
+    poopSprite.style.top = randomNumber(15,30) + "vh";
     poopSprite.style.display = "block";
   }
   else if (fecesType == 2){
     console.log("poop2");
-    peeSprite.style.left = randomNumber(50, 850) + "px";
-    peeSprite.style.top = randomNumber(20,250) + "px";
+    peeSprite.style.left = randomNumber(10, 30) + "vw";
+    peeSprite.style.top = randomNumber(5,20) + "vh";
     peeSprite.style.display = "block";
   }
 }
@@ -50,26 +53,38 @@ let poopPickup = (type) => {
     poopSprite.style.display = "none";
   }
   else if (type == 2) {
-    peeSprite.style.display = "none;"
+    peeSprite.style.display = "none"
   }
 }
 
 let replenish = (type) => {
   if(type == 1 ) {
+    console.log("succ1");
+    foodBowl.src="dopFood.jpg";
+    dog.className = 'dogSprite dogSpriteAnimFeed'
     coins -= 20;
+    console.log("succ2");
     hunger += 30;
     if (coins < 0) { coins = 0;};
     if (hunger > 100) { hunger = 100;};
     hungerProgress.style.width = hunger + "%";
     coinTab.textContent = coins
+    setTimeout(function(){
+      replenishReversal(1);
+    }, 10000)
   }
   else if (type == 2) {
+    waterBowl.src="dogWater.jpg";
+    dog.className = 'dogSprite dogSpriteAnimFeed'
     coins -= 10;
     thirst += 30;
     if (coins < 0) { coins = 0;};
     if (thirst > 100) { thirst = 100;}
     thirstProgress.style.width = thirst + "%";
     coinTab.textContent = coins
+    setTimeout(function(){
+      replenishReversal(2);
+    }, 10000)
   }
   else if (type == 3) {
     coins -= 20;
@@ -80,5 +95,16 @@ let replenish = (type) => {
     coinTab.textContent = coins
   }
 };
-setInterval(time, 10000);
+let replenishReversal = (type) => {
+  if (type == 1) {
+    console.log("succ3");
+    foodBowl.src="emptybowl.jpg";
+    dog.className = 'dogSprite dogSpriteAnimMain'
+  }
+  else if (type == 2){
+    waterBowl.src="emptybowl.jpg";
+    dog.className = 'dogSprite dogSpriteAnimMain'
+  }
+};
+setInterval(time, 15000);
 setInterval(poopGen,40000);
